605a545bd4d813d0f17c0d0a0a55c73105d7d7f1
Josediaz777 <manueltiscareno1200@gmail.com>
2020-10-27 19:36:57 -0600

    commit Articulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { TelefoniaComponent } from './components/telefonia/telefonia.component';
+import { LineablancaComponent } from './components/lineablanca/lineablanca.component';
+import { CamarasComponent } from './components/camaras/camaras.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    TelefoniaComponent,
+    LineablancaComponent,
+    CamarasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component:HomeComponent},
+      {path: 'home', component:HomeComponent},
+      {path: 'telefonia', component:TelefoniaComponent},
+      {path: 'camaras', component:CamarasComponent},
+      {path: 'lineablanca', component:LineablancaComponent},
+      {path: 'about', component:AboutComponent}
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
